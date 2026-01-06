6e01c99fca0ccd005c974c58e41e42bb3f6f4646
RIZWAAN MS <msrizwaan1234@outlook.com>
2026-01-06 19:24:43 +0530

    bug fixing

--- lib/Music Player/dynamic_background.dart
+++ lib/Music Player/dynamic_background.dart
@@ -1,14 +1,5 @@
-import 'dart:async';
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
-import 'package:video_player_win/video_player_win.dart';
-import 'package:music_controller/Settings/settings.dart';
-import 'package:weather_animation/weather_animation.dart';
 import '../Managers/weather_manager.dart';
-
-late dynamic controller_darkmode;
-late dynamic controller_lightmode;
 
 class WeatherBackground extends StatefulWidget {
   const WeatherBackground({super.key});
