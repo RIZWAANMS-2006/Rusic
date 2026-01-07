3ad2f39275ebbe6b16013cbcd0a5f187bfea770a
RIZWAAN MS <msrizwaan1234@outlook.com>
2026-01-07 20:38:38 +0530

    settingsmanager

--- lib/Managers/settings_manager.dart
+++ lib/Managers/settings_manager.dart
@@ -8,18 +8,33 @@ class SettingsManager {
   }
 
   //Get Functions
-
   static bool get getDarkMode => userSettings.getBool('darkMode') ?? true;
-  static String get getFontFamily => userSettings.getString('fontFamily') ?? "normal";
-  
+  static String get getFontFamily =>
+      userSettings.getString('fontFamily') ?? "normal";
+  static String get getLastSongPlayedName =>
+      userSettings.getString('lastSongPlayedName') ?? "No Song is Playing...";
+  static String get getLastSongPlayedPath =>
+      userSettings.getString('lastSongPlayedPath') ?? "";
 
   //Set Functions
-
-  static Future<void> setDarkMode(bool value) async{
+  static Future<void> setDarkMode(bool value) async {
     await userSettings.setBool('darkMode', value);
   }
 
-  static Future<void> setFontFamily(String value) async{
+  static Future<void> setFontFamily(String value) async {
     await userSettings.setString('fontFamily', value);
+  }
+
+  static Future<void> setLastSongPlayedName(String value) async {
+    await userSettings.setString('lastSongPlayedName', value);
+  }
+
+  static Future<void> setLastSongPlayedPath(String value) async {
+    await userSettings.setString('lastSongPlayedPath', value);
+  }
+
+  static Future<void> setDefaults() async {
+    await userSettings.setBool('darkMode', true);
+    await userSettings.setString('fontFamily', "normal");
   }
 }
